549c4c0f9ae03face3dcebe4ed84e1c3bc1a1b4c
Rahul Ravindran <ravindranrahul@users.noreply.github.com>
2023-10-12 13:00:32 +0530

    feat: initial release

---
+++ README.md
@@ -0,0 +1,27 @@
+# Foundryize
+
+Tiny Bash script to automate the process of converting your existing Solidity projects to work with Foundry. Compatible with hardhat. Adapted from [foundry book](https://book.getfoundry.sh/config/hardhat#use-foundry-in-an-existing-hardhat-project).
+
+## Usage
+
+Make sure you have Bash and curl installed on your system.
+
+```bash
+curl -L https://raw.githubusercontent.com/ravindranrahul/foundryize/main/foundryize.sh | bash
+```
+
+The script will guide you through the process and provide instructions for the next steps.
+
+## License
+
+This project is licensed under the MIT License. See the LICENSE file for details.
+
+## Contributing
+
+Contributions are welcome! We appreciate your help in making Foundryize even better. Here are a few ways you can contribute:
+
+- Open an issue to report a bug or suggest an enhancement.
+- Create a pull request with improvements or fixes.
+- Provide feedback and ideas to help shape the project's direction.
+
+Looking forward to your contributions and are happy to have you on board!
---
+++ foundryize.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+# Prompt the user for the folder where Solidity files are stored
+echo "Please enter the path to the folder containing the contract files:"
+read contracts_folder
+
+# Check if the contracts folder exists
+if [ ! -d "$contracts_folder" ]; then
+  echo "Error: '$contracts_folder' folder does not exist."
+  exit 1
+fi
+
+# Print the folder path for confirmation
+echo "You entered: $contracts_folder"
+
+# Check if 'forge' command is installed
+if ! command -v forge &>/dev/null; then
+  echo "Foundry is not installed. Installing it now..."
+  # Install Foundry using curl and bash
+  curl -L https://foundry.paradigm.xyz | bash
+else
+  echo "Foundry is already installed."
+fi
+
+# Initialize Foundry with 'forge init'
+forge init temp_foundry_install
+
+# Copy the "lib" folder from the temporary installation directory to the current directory
+cp -r "temp_foundry_install/lib" ./lib
+
+# Clean up the temporary installation directory
+rm -rf "temp_foundry_install"
+
+# Check if 'remappings.txt' exists in the current directory
+if [ -e "remappings.txt" ]; then
+  echo "Using existing remappings.txt. Please ensure it is up to date for Foundry to work correctly."
+else
+  echo "Auto-generating remappings.txt..."
+  # Generate a new remappings.txt using 'forge remappings'
+  forge remappings > remappings.txt
+fi
+
+# Create a foundry.toml file with the specified content
+cat <<EOL > foundry.toml
+[profile.default]
+src = "$contracts_folder"
+out = 'foundry-out'
+libs = ['node_modules', 'lib']
+test = 'foundry-test'
+cache_path  = 'foundry-cache'
+
+# See more config options https://book.getfoundry.sh/reference/config.html
+EOL
+
+# Inform the user about the next steps
+echo "All set! Go forth and code like a Foundry ninja. 💻🚀"
+echo "Next steps: Create tests in 'foundry-test' and run 'forge test'."
+echo "Learn more: Visit https://book.getfoundry.sh/"
